ae037b2cbfece0affc500e1bc2c04c427fb29bef
ksk001100 <hm.pudding0715@gmail.com>
2019-09-07 22:11:36 +0900

    commit

--- src/lib/download_manager.rs
+++ src/lib/download_manager.rs
@@ -2,15 +2,13 @@ use crate::lib::utils::{Download, get_content_length};
 use crate::lib::downloader::parallel::ParallelDownloader;
 use crate::lib::downloader::single::SingleDownloader;
 
-pub struct DownloadManager
-{
+pub struct DownloadManager {
     pub downloader: Box<Download>
 }
 
-impl DownloadManager
-{
+impl DownloadManager {
     pub fn new(url: String) -> Self {
-        let downloader: Box<Download>= {
+        let downloader: Box<Download> = {
             if get_content_length(&url) < 1000000 {
                 Box::new(SingleDownloader { url })
             }
